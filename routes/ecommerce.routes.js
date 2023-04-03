b51a2edab33f987e6bc50e89f0afadc2cb601843
J12003LPZ <104168530+J12003LPZ@users.noreply.github.com>
2023-04-03 14:39:55 -0400

    Update ecommerce.routes.js

--- routes/ecommerce.routes.js
+++ routes/ecommerce.routes.js
@@ -8,12 +8,21 @@ import {
   createAuction,
   deleteAuction,
   updateAuction,
+  getAllMenProducts,
+  getAllWomenProducts,
+  getAllGirlsProducts,
+  getAllKidsProducts,
 } from "../controllers/ecommerce.controllers.js";
+
 const router = Router();
 
 router.get("/products", getProducts);
 router.get("/product/:id", getProduct);
 router.get("/products/latest", getLatestProducts);
+router.get("/products/men", getAllMenProducts);
+router.get("/products/woman", getAllWomenProducts);
+router.get("/products/kids", getAllKidsProducts);
+router.get("/products/girls", getAllGirlsProducts);
 router.get("/auctions", getAuctions);
 router.get("/aunction/:id", getAunction);
 router.post("/aunction", createAuction);
