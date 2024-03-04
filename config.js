1950516ed686bc64d8e77450a9bc12187c361e52
J12003LPZ <104168530+J12003LPZ@users.noreply.github.com>
2024-03-03 18:37:38 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_DATABASE = "pocket-ecommerce";
-export const DB_USER = "bv3eoxya7mixmbfn90s8";
+export const DB_DATABASE = "sql5688384";
+export const DB_USER = "sql5688384";
 export const DB_PASSWORD =
-  "pscale_pw_ojPr0c7xO2p2rXc6ist97jsUAmJIbax8gg38eBV1G9i";
-export const DB_HOST = "us-east.connect.psdb.cloud";
+  "v2pk7rAEPn";
+export const DB_HOST = "sql5.freesqldatabase.com";
 export const DB_PORT = 3306;
